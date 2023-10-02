ba493ea1c4f75769f802c4aceb7c8b8269a7b63e
Timo Leiding <timo.leiding@gmx.de>
2023-10-02 17:11:18 +0200

    Added folder for slot machine and jsbasic

---
+++ slotmachine/project.js
@@ -0,0 +1,17 @@
+// 1. Deposit some mone
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Payout or take bet
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+import prompt from "prompt-sync";
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+}
+
+deposit();
